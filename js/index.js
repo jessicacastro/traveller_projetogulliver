18922b912e8c73b3807b8c074e8b6cc11a8b51f8
Ewerton I. Barreto <iohrannewerton@gmail.com>
2021-03-31 00:03:34 -0300

    Modal no botão de login
    Adicionei o modal e o form de login dentro dele, tudo formatado e responsivo.

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// ABRIR E FECHAR MODAL LOGIN
+
+function openModal() {
+  document.getElementById('modal').style.top = "50%";
+}
+
+function closeModal() {
+  document.getElementById('modal').style.top = "-100%";
+}
+
