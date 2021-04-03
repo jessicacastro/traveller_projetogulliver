0dbae04df674fdf1e4822c0bab710171fac47c78
Jessica Castro <jessica.castros@outlook.com>
2021-04-02 21:34:28 -0300

    Scroll, css da pagina

--- js/index.js
+++ js/index.js
@@ -8,3 +8,7 @@ function closeModal() {
   document.getElementById('modal').style.top = "-100%";
 }
 
+const goTo = (element) => {
+  const el = document.querySelector(element);
+  el.scrollIntoView({ behavior: 'smooth' })
+}
